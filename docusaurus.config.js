5331179e00f74408c94ef084e4ef5ccfa499a14d
shubhamAW <waykarshubham2022@gmail.com>
2022-11-01 19:09:29 +0530

    update config.js file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,7 +150,7 @@ const config = {
           //   label:'Languages',
           //   to:'/Languages/intro'
           // },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           // {
           //   href: 'https://github.com/shubhamAW/gcoejexamprep/',
           //   label: 'GitHub',
